59a048e12e957ea74c31422e2c0507f2f3856030
Lunixyz <otakinhuhdev@gmail.com>
2023-02-15 07:57:34 -0300

    1.0.1

--- src/Constructors/Button.ts
+++ src/Constructors/Button.ts
@@ -1,27 +1,20 @@
 const { MessageActionRow, MessageButton } = require("discord.js");
 class Button {
-  id: string[];
-  name: string[];
-  style: string[];
   /**
    * @param {Array} id button custom ID
    * @param {Array} name button name
    * @param {Array} style style of the button (PRIMARY, SECONDARY, SUCCESS, DANGER, LINK)
    * @param {Array} emoji unicode emojis
    */
-  constructor(id: string[], name: string[], style: string[]) {
-    this.id = id;
-    this.name = name;
-    this.style = style;
-  }
-  builder() {
+
+  builder(id: string[], name: string[], style: string[]) {
     const button = new MessageActionRow();
-    for (let i = 0; i < this.name.length; i++) {
+    for (let i = 0; i < name.length; i++) {
       button.addComponents(
         new MessageButton()
-          .setLabel(`${this.name[i]}`)
-          .setCustomId(`${this.id[i]}`)
-          .setStyle(`${this.style[i]}`)
+          .setLabel(name[i])
+          .setCustomId(id[i])
+          .setStyle(style[i])
       );
     }
     return button;
